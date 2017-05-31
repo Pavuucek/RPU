cc6084b4e634c66dca0c0467e4f7a27768f77a97
Michal Kuncl <michal.kuncl@gmail.com>
2017-06-01 00:51:40 +0200

    add ProcessM3u

---
+++ ProcessM3u/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ProcessM3u
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var i = File.ReadAllText("aplaylist.m3u");
+            var lines = Regex.Split(i, "\r\n|\r|\n");
+            var aname = string.Empty;
+            var sb = new StringBuilder();
+            foreach (var line in lines)
+            {
+                if (line.StartsWith("#EXTINF:-1,"))
+                {
+                    aname = line.Replace("#EXTINF:-1,", string.Empty);
+                    sb.AppendLine(line);
+                }
+                else if (line.StartsWith("http://"))
+                {
+                    var s = line.Replace("http://",
+                        string.Format(
+                            "pipe:///usr/bin/avconv -loglevel fatal -metadata service_name={0} -vcodec copy -acodec copy -f mpegts pipe:1 -i $$H$$",
+                            aname));
+                    sb.AppendLine(s.Replace("$$H$$", "http://"));
+                }
+                else sb.AppendLine(line);
+            }
+            File.WriteAllText("playlist.m3u",sb.ToString());
+
+        }
+    }
+}
